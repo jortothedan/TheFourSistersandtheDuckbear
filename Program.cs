b57128c00fb0fce91212838c9d46954b0a294e33
Jordan Garcia <jordan.garcia.roche@gmail.com>
2022-06-07 11:40:02 -0400

    some math using variables

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace TheFourSistersandtheDuckbear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int chocolateEggs;
+             int girlsEggs;
+             int duckBearEggs;
+
+             Console.WriteLine("How many chocolate eggs did you girls collect today?");
+             chocolateEggs = Convert.ToInt32(Console.ReadLine());
+
+
+             duckBearEggs = chocolateEggs % 4;
+             girlsEggs = (chocolateEggs - duckBearEggs) / 4;
+
+             Console.WriteLine("Girls, you each get " + girlsEggs + " chocolate eggs today...");
+
+             Console.WriteLine("And Duckbear gets: " + duckBearEggs + " chocolate eggs today!");
+        }
+    }
+}
